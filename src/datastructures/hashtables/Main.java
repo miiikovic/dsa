ba483fcfa8b1051c8c58d075c55c6f5a54eeb647
Momcilo Mikovic <miiikovic19@gmail.com>
2023-05-03 21:55:28 +0200

    added hashtables

---
+++ src/datastructures/hashtables/Main.java
@@ -0,0 +1,21 @@
+package datastructures.hashtables;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HashTable myHashTable = new HashTable();
+
+        myHashTable.set("nails", 100);
+        myHashTable.set("tile", 50);
+        myHashTable.set("lumber", 80);
+
+        myHashTable.set("bolts", 200);
+        myHashTable.set("screws", 140);
+
+//        myHashTable.printTable();
+        System.out.println(myHashTable.get("lumber"));
+        System.out.println(myHashTable.get("flowers"));
+
+        System.out.println(myHashTable.keys());
+    }
+}
