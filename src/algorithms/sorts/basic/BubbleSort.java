902dd9a618699ef61243710a3cab0fda0d42f2a0
Momcilo Mikovic <miiikovic19@gmail.com>
2023-05-08 20:38:07 +0200

    added bubble sort

---
+++ src/algorithms/sorts/basic/BubbleSort.java
@@ -0,0 +1,31 @@
+package algorithms.sorts.basic;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
+            for(int j = 0; j < i; j++) {
+                if (array[j] > array[j+1]) {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void selectionSort(int[] array) {
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] myArray = {4,2,6,5,1,3};
+
+        bubbleSort(myArray);
+
+        System.out.println(Arrays.toString(myArray));
+    }
+}
