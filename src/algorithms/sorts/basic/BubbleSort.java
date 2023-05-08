6aac99bae63dd7180dbd526f625520ec08582904
Momcilo Mikovic <miiikovic19@gmail.com>
2023-05-08 21:17:30 +0200

    added insertion sort

--- src/algorithms/sorts/basic/BubbleSort.java
+++ src/algorithms/sorts/basic/BubbleSort.java
@@ -16,22 +16,6 @@ public class BubbleSort {
         }
     }
 
-    public static void selectionSort(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            int minIndex = i;
-            for (int j = i+1; j < array.length; j++) {
-                if (array[j] < array[minIndex]) {
-                    minIndex = j;
-                }
-            }
-            if (i != minIndex) {
-                int temp = array[i];
-                array[i] = array[minIndex];
-                array[minIndex] = temp;
-            }
-        }
-    }
-
     public static void main(String[] args) {
 
         int[] myArray = {4,2,6,5,1,3};
