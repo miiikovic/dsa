28f40ab308a35572f66b4768f31f854af0d87174
Momcilo Mikovic <miiikovic19@gmail.com>
2023-05-09 14:02:14 +0200

    added merge sort

--- src/algorithms/sorts/basic/BubbleSort.java
+++ src/algorithms/sorts/basic/BubbleSort.java
@@ -6,7 +6,7 @@ public class BubbleSort {
 
     public static void bubbleSort(int[] array) {
         for (int i = array.length - 1; i > 0; i--) {
-            for(int j = 0; j < i; j++) {
+            for(int j = 0; j <= i-1; j++) {
                 if (array[j] > array[j+1]) {
                     int temp = array[j];
                     array[j] = array[j+1];
